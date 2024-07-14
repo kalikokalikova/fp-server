e10b189f0566ff4727860767f9dfa36d428514b3
kalikokalikova <calico.seders@gmail.com>
2024-07-14 13:11:03 -0700

    * works, so I know it's something about this origins setting

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "*"
-]
+    "http://localhost:5173",
+    "https://fp-client-107bc916594c.herokuapp.com"
+    ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
