983abd7ffd160e711733b91f6e62eab33c8a7c55
kalikokalikova <calico.seders@gmail.com>
2024-07-14 12:44:13 -0700

    basic cors for frontend

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return 'Hello world!'
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get('/events')
+def events():
+    return ["event42", "event69", "event007"]
