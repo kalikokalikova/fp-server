946f03a76be66689cb2fbaaa17602fe4856d8cbb
anne <aburkhardt@seiumn.org>
2024-09-03 16:41:44 -0500

    solved the datetime issue

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,12 @@
 # This is for data validation
 from typing import Union, Optional
-from datetime import datetime
+from datetime import datetime as dt
 from pydantic import BaseModel
 
 class EventBase(BaseModel):
     title : str
     description : str | None = None
-    datetime: datetime
+    datetime: dt
     location_name: str | None = None
     is_public: bool
     image_url: str
@@ -18,7 +18,7 @@ class EventCreate(EventBase):
 class EventUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    #datetime: Optional[datetime] = None #variable not allowed in type expression?
+    datetime: Optional[dt] = None 
     location_name: Optional[str] = None
     is_public: Optional[bool] = None
     image_url: Optional[str] = None    
